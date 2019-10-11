ee7ed5b35fb48bf645f32308aabc32a829a42b94
98kevin <kevinfuentes201730303@cunoc.edu.gt>
2019-10-11 01:03:43 -0600

    Lectura de la informacion en el servidor

--- src/public/ingreso.js
+++ src/public/ingreso.js
@@ -1,8 +1,13 @@
+//captura de los elementos del documento 
 const input = document.getElementById('file');
 const editor = document.getElementById('contenido');
+const botonEnviar = document.querySelector("#botonEnviar");
+
+//Evento del boton enviar
+botonEnviar.addEventListener('click', evaluarTexto);
+
 
 input.addEventListener('change', function () {
-    alert("Evaluando funcio")
   if (input.files.length > 0) {
     readFile(input.files[0]);
   }
@@ -15,3 +20,24 @@ function readFile(file) {
   }
   reader.readAsText(file);
 }
+
+var URL = 'http://localhost:3000/evaluarContenido';
+
+function handleSuccess(data) { console.log(data); }
+function handleFailure(data) { console.log('error', data); }
+
+//Funcion del boton enviar
+function evaluarTexto(){
+  console.log('Funcion evaularTExto Activada');
+    // POST
+  axios.post(URL, {
+    textoDeIngreso: editor.value
+  })
+  .then(handleSuccess)
+  .catch(handleFailure);
+};
+
+// GET
+axios.get(URL, { params: BODY })
+  .then(handleSuccess)
+  .catch(handleFailure);
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,45 +12,11 @@ router.get('/ingreso', (req, res) => {
     res.render('ingreso', { max: 15 });
 });
 
-router.get('/postdata', (req, res) => {
-    console.log("recibio");
-    let data = req.query.format;
-    console.log(data);
 
-
-});
-
-router.post('/postusers', (req, res) => {
-    console.log("recibo usuario");
-    console.log(req);
-    console.log(req.body.firstName);
-    console.log(req.body.lastName);
-    console.log(req.body.text);
-
-
-    res.status(400).send('wrong');
-});
-
-router.get('/users', (req, res) => {
-    console.log("envio usuario");
-
-    setTimeout(() => {
-        res.status(400).json({
-            usurio: 'oliver',
-            id: '4',
-            carne: '202'
-        });
-    }, 4500);
-    /*
-        setTimeout(() => {
-            res.status(200).json({ usuario: 'oliver sierra' });
-        }, 3500);*/
-});
-
-router.get('/hola', (req, res) => {
-    res.render('saludo', { max: 15 });
-    //res.sendFile(path.join(__dirname,'/views/saludo.html'));
-    //res.sendFile(path.join(__dirname,'../views/saludo.html'));
+router.post('/evaluarContenido', (request,response) => {
+    console.log('Podes hacer lo que queras con esta informacion');
+    console.log(request.body.textoDeIngreso.value);
+    response.status(200).send('exito');
 });
 
 module.exports = router;
